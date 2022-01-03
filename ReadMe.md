396793949c2d3d5de21cfe596491eb13fb9d202f
Anthony Sneed <tony.sneed@icloud.com>
2022-01-03 17:12:23 -0600

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,25 @@
+﻿# EventDriven.DependencyInjection
+
+Helper methods for configuring services with dependency injection.
+
+#### Usage
+
+- In a Web API project.
+
+```csharp
+builder.Services.AddAppSettings<MyAppSettings>(builder.Configuration);
+```
+
+- In a console project.
+
+```csharp
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        var config = services.BuildServiceProvider()
+            .GetRequiredService<IConfiguration>();
+        services.AddAppSettings<MyAppSettings>(config);
+    })
+    .Build();
+```
---
+++ src/EventDriven.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace EventDriven.DependencyInjection
+{
+    /// <summary>
+    /// Helper methods for configuring services with dependency injection.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register app settings with dependency injection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the service to.</param>
+        /// <param name="config">The application's <see cref="IConfiguration"/>.</param>
+        /// <typeparam name="TAppSettings">Strongly typed app settings class.</typeparam>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddAppSettings<TAppSettings>(
+            this IServiceCollection services, IConfiguration config)
+            where TAppSettings : class
+        {
+            services.Configure<TAppSettings>(
+                config.GetSection(typeof(TAppSettings).Name));
+            services.AddTransient(sp =>
+                sp.GetRequiredService<IOptions<TAppSettings>>().Value);
+            return services;
+        }
+    }
+}
